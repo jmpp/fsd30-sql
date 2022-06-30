9d350d78f058ecec9eb0ad6e416116dace6e950c
J.M. Cléry <jmclery@gmail.com>
2022-06-30 13:08:36 +0200

    Updates chap6 corrections

--- Chap5_DQL/corrections/chap6.sql
+++ Chap5_DQL/corrections/chap6.sql
@@ -1,30 +1,67 @@
-ALTER TABLE pilots ADD lead_pl VARCHAR(255) DEFAULT NULL AFTER certificate;
+-- =========================================
+-- 01 Exercice ajouter une clé étrangère
+-- =========================================
 
+-- Ajoutez la clé étrangère "lead_pl" dans la table pilots.
 
-ALTER TABLE pilots ADD CONSTRAINT fk_lead_pilot
-FOREIGN KEY (`lead_pl`) REFERENCES `pilots` (`certificate`)
-ON DELETE RESTRICT
-ON UPDATE RESTRICT;
+ALTER TABLE pilots
+ADD
+    lead_pl VARCHAR(255) DEFAULT NULL AFTER certificate;
+
+ALTER TABLE pilots
+ADD
+    CONSTRAINT fk_lead_pilot FOREIGN KEY (`lead_pl`) REFERENCES `pilots` (`certificate`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+-- Mettez à jour la table pilots sachant que Pierre est le chef pilote de Alan, Tom et Yi. Et que John est le chef pilote de Sophie, Albert et Yam.
 
 UPDATE pilots
 SET lead_pl = 'ct-7'
-WHERE name IN ('Alan', 'Tom', 'Yi');
+WHERE
+    name IN ('Alan', 'Tom', 'Yi');
 
--- Avec une sous-table temporaire, créée pour l'occasion
+
+-- (Avec une sous-table temporaire, créée pour l'occasion :)
 UPDATE pilots
 SET lead_pl = (
-        SELECT * FROM (
-                SELECT pp.certificate
+        SELECT *
+        FROM (
+                SELECT
+                    pp.certificate
                 FROM pilots pp
-                WHERE pp.name = 'John'
+                WHERE
+                    pp.name = 'John'
             ) tempTable
     )
 WHERE
     name IN ('Sophie', 'Albert', 'Yan');
 
--- (SELECT pp.certificate FROM pilots pp WHERE pp.name = 'Pierre')
 
-
+-- Ecrire et exécuter une requête pour déterminer les pilotes qui n'ont pas de chef.
 SELECT * FROM pilots WHERE lead_pl IS NULL;
 
------ 02 Jointures
+
+-- =========================================
+-- 02 Exercice Jointures
+-- =========================================
+
+
+-- Sélectionnez les certificats des pilotes et les noms des compagnies pour lesquelles ils travaillent.
+
+SELECT
+    pilots.certificate AS pilot_certificate,
+    companies.name AS companie_name
+FROM `pilots`
+    INNERT JOIN companies ON pilots.company = companies.comp;
+
+-- Sélectionnez les certificats et les noms des pilotes travaillant pour la compagnie 'Air France', et ayant fait plus de 60 heures de vol.
+
+
+SELECT
+    pilots.name,
+    company,
+    pilots.certificate
+FROM pilots
+    INNER JOIN companies on pilots.company = companies.comp
+WHERE
+    company In('FRE1')
+    and pilots.num_flying > 60;
